9c2366ec4a3020a8824326fa04d4df51893c8b4f
Marek Fodor <fodormarek5@gmail.com>
2025-05-20 09:23:26 +0200

    feat(env): Add web_url to env types.

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
   readonly STRAPI_API_HOSTNAME: string;
   readonly STRAPI_API_PORT: string;
   readonly STRAPI_API_PREFIX: string;
+  readonly WEB_URL: string;
 }
 
 interface ImportMeta {
