1e332673d31b0ffcaa8861a9c722a2d3465f74a9
Marek Fodor <fodormarek5@gmail.com>
2025-05-21 15:37:54 +0200

    feat(ui): Add not-functioning contact form.

---
+++ frontend/src/components/contact-form.tsx
@@ -0,0 +1,64 @@
+export default function ContactForm() {
+  const handleSubmit = async () => {};
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-6">
+      <div>
+        <label
+          htmlFor="name"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Full Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          placeholder="Your Name"
+          className="input-field w-full border border-border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary transition"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Email Address
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          placeholder="you@example.com"
+          className="input-field w-full border border-border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary transition"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="message"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          rows={5}
+          required
+          placeholder="Your message..."
+          className="input-field w-full border border-border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary transition"
+        ></textarea>
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="button-primary w-full text-lg disabled:opacity-70"
+        >
+          Send Message
+        </button>
+      </div>
+    </form>
+  );
+}
