324bd4d438496ce62d65bbdc943140c9c0134d6f
Marek Fodor <fodormarek5@gmail.com>
2025-05-22 19:11:21 +0200

    feat(frontend): Add analytics.

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly STRAPI_API_PORT: string;
   readonly STRAPI_API_PREFIX: string;
   readonly WEB_URL: string;
+  readonly UMAMI_SCRIPT_URL: string;
+  readonly UMAMI_WEBSITE_ID: string;
 }
 
 interface ImportMeta {
